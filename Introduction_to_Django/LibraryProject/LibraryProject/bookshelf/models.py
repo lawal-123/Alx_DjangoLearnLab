a7eb7951318476cb3a5b0a6390d6ae58983aa579
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2024-11-15 19:45:41 +0100

    Update models.py

--- Introduction_to_Django/LibraryProject/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/bookshelf/models.py
@@ -2,9 +2,10 @@
 from django.db import models
 
 class Book(models.Model):
-        title = models.CharField(max_length=200)
-        author = models.CharField(max_length=100)
-        publication_year = models.IntegerField()
+    def __init__(self, title, author, publication_year)
+        self.title = models.CharField(max_length=200)
+        self.author = models.CharField(max_length=100)
+        self.publication_year = models.IntegerField()
 
 def __str__(self):
     return f"{self.title} by {self.author} ({self.publication_year})"
