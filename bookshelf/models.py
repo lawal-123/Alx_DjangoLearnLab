ee7a8f68bdd81159c850f06f5cf00cbb0feb9a47
Lawal Abdullah <sirojudeenopeyemi@gmail.com>
2024-11-17 16:32:42 +0100

    Add libraryProject

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,6 +1,4 @@
-    # bookshelf/models.py
 from django.db import models
-
 class Book(models.Model):
         title = models.CharField(max_length=200)
         author = models.CharField(max_length=100)
