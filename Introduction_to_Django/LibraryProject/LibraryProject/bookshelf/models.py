39038bfd9c8aaa78d7691b36bf2e0eda321a62d5
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2024-11-17 18:25:53 +0100

    Update models.py

--- Introduction_to_Django/LibraryProject/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/bookshelf/models.py
@@ -5,5 +5,5 @@ class Book(models.Model):
     publication_year = models.DateField()
 
 def __str__(self):
-    return f"{self.title} by {self.author} ({self.publication_year})"
+    return self.title
 print(book)
