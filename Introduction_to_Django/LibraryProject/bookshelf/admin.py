5fffeec2fd0c2195ad74c493ac8c75984f15bb70
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2024-11-17 23:13:08 +0100

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from .models import Book
 admin.site.register(Book)
+
+
+list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
