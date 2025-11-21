aef0d5f613e09aad28a99a200283c79c3a70e086
lawal abdullah <gallant123@gmail.com>
2025-11-21 21:39:41 +0100

    Make changes to views.py and urls.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,36 +2,23 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+# --- FIX: Ensure Library model is imported ---
+from .models import Book, Library 
 
 # --- Function-based View ---
-
-def list_all_books(request):
-    """
-    Lists all Book objects in the database and renders them using a template.
-    This view demonstrates a simple list query (Book.objects.all()).
-    """
-    # Query all books, ordering by title
-    all_books = Book.objects.select_related('author').order_by('title')
-    
-    context = {
-        'books': all_books
-    }
-    
-    # Renders the HTML template list_books.html
-    return render(request, 'relationship_app/list_books.html', context)
-
+# ... (list_all_books function remains the same)
 
 # --- Class-based View (DetailView) ---
 
 class LibraryDetailView(DetailView):
-    # Model to query
-    model = Library
+    """
+    Displays details for a specific Library, including all books associated 
+    via the ManyToManyField.
+    """
+    # Model to query - Requires the 'Library' model to be imported.
+    model = Library 
     
-    # Template to use
     template_name = 'relationship_app/library_detail.html'
-    
-    # The URL pattern will use 'pk' (primary key) to look up the library object
     context_object_name = 'library'
 
     def get_queryset(self):
