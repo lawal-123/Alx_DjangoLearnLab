84b474df6f9710a56815162fd653848c3759c2b0
lawal abdullah <gallant123@gmail.com>
2025-11-21 21:43:02 +0100

    make changes to views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,11 +2,25 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-# --- FIX: Ensure Library model is imported ---
-from .models import Book, Library 
+from .models import Book, Library
 
 # --- Function-based View ---
-# ... (list_all_books function remains the same)
+
+def list_all_books(request):
+    """
+    Lists all Book objects in the database and renders them using a template.
+    This view demonstrates a simple list query (Book.objects.all()).
+    """
+    # Query all books, ordering by title
+    all_books = Book.objects.select_related('author').order_by('title')
+    
+    context = {
+        'books': all_books
+    }
+    
+    # Renders the HTML template list_books.html
+    return render(request, 'relationship_app/list_books.html', context)
+
 
 # --- Class-based View (DetailView) ---
 
@@ -14,13 +28,21 @@ class LibraryDetailView(DetailView):
     """
     Displays details for a specific Library, including all books associated 
     via the ManyToManyField.
+    This view demonstrates retrieving a single object and accessing its 
+    ManyToManyField relationship (library.books.all()).
     """
-    # Model to query - Requires the 'Library' model to be imported.
-    model = Library 
+    # Model to query
+    model = Library
     
+    # Template to use
     template_name = 'relationship_app/library_detail.html'
+    
+    # The URL pattern will use 'pk' (primary key) to look up the library object
     context_object_name = 'library'
 
+    # Optimization: Use select_related/prefetch_related for associated data.
+    # Since 'books' is ManyToMany, we use prefetch_related.
+    # We also prefetch the author for each book to avoid N+1 queries in the template.
     def get_queryset(self):
         return Library.objects.prefetch_related(
             'books__author'
