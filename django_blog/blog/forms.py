99f5c761f37429cabfb4edb0dc96c5b55ac31231
Lawal Abdullah Folarin <sirojudeenopeyemi@gmail.com>
2025-12-08 01:23:54 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+TagWidget()
 class BlogUserCreationForm(UserCreationForm):
     # Add email field and make it required
     email = forms.EmailField(required=True) 
