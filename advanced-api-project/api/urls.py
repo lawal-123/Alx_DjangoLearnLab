95928c410d9470dc8f796251b904c956c7926630
lawal abdullah <gallant123@gmail.com>
2026-01-01 13:25:33 +0100

    Update advanced_api_project

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
-    path('api/', include('api.urls')),
+    
 ]
