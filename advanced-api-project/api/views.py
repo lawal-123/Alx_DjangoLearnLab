371be67e2992583b13597eee40dffe6ede74b1d5
lawal abdullah <gallant123@gmail.com>
2026-01-01 17:58:47 +0100

    Update advanced_api_project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,12 +3,20 @@ from .models import Book
 from .serializers import BookSerializer
 from datetime import datetime
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework 
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # Public access
+    permission_classes = [permissions.AllowAny] 
+    
+    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
+    # Public access
 
 # DetailView: Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
